756553384935ca6d8c976490a53832bfdaf65a57
Lisa Lee <lisaleekallman@gmail.com>
2021-10-09 18:34:26 +0200

    removed unused comments, edited image sources

--- js/drawer.js
+++ js/drawer.js
@@ -15,7 +15,7 @@ let currentX2 = null
 let currentY2 = null
 
 let i = 0
-const images = ["../images/image1.jpg", "../images/image2.jpg", "../images/image3.jpg", "../images/image4.jpg", "../images/image5.jpg"].map(src => {
+const images = ["../images/image1.jpeg", "../images/image2.jpeg", "../images/image3.jpeg", "../images/image4.jpeg", "../images/image5.jpeg"].map(src => {
     const image = document.createElement("img")
     image.src = src
     return image
