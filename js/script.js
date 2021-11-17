80bc16118d1258f013d241868de175c2f6e2f33c
Lisa Lee <lisaleekallman@gmail.com>
2021-11-17 20:48:35 +0100

    dimension correction letter box

--- js/script.js
+++ js/script.js
@@ -10,23 +10,19 @@ for (let i = 0; i < letters.length; i++) {
     letter.classList.add("letter");
     letterBox.appendChild(letter);
 }
-letterBox.addEventListener("mousemove", function (event) {
+document.addEventListener("mousemove", function (event) {
     const x = event.pageX;
     const y = event.pageY;
 
-    document.querySelectorAll(".letter").forEach(div => {
-        // find the distance for every letter div
-        const dx = div.offsetLeft + 10 - x;
-        // every div 100x100 (-x is to ignore the cursor)
-        const dy = div.offsetTop + 10 - y;
+    document.querySelectorAll(".letter").forEach(p => {
+        let factor = p.offsetLeft / 2060
+        const dx = (document.body.clientWidth * factor) + 10 - x;
+        const dy = p.offsetTop + 10 - y;
         const dist = Math.sqrt(dx * dx + dy * dy);
-        // distance(diagonal) is a^2 * b^2 = c^2
         const score = Math.exp(dist * -0.01);
-        // convert the distance to a "score" between 0 - 1 through an exponential-curve
 
-        // div.innerHTML = score.toFixed(2)
-        div.style.transform = "scale(" + 3 * score + ")";
-        div.style.fontWeight = (200 * Math.round(8 * score));
+        p.style.transform = "scale(" + 3 * score + ")";
+        p.style.fontWeight = (200 * Math.round(8 * score));
     });
 });
 
@@ -43,8 +39,8 @@ function runClock() {
     const minHand = clock.querySelector('.minutes');
     const secHand = clock.querySelector('.seconds');
 
-    let hourRotation = 360 / 12 * hour + 0.5 * min;
-    let minRotation = 6 * min + 0.1 * sec;
+    let hourRotation = 360 / 12 * hour + 0.5 * min - 90;
+    let minRotation = 6 * min + 0.1 * sec - 90;
     let secRotation = 6 * sec + 0.006 * ms;
 
     hourHand.style.transform = 'rotate(' + hourRotation + 'deg)';
@@ -391,36 +387,416 @@ function animate2() {
 }
 animate2();
 
-
-// // form
-// var form = document.getElementById("my-form");
-
-// async function handleSubmit(event) {
-//     event.preventDefault();
-//     var status = document.getElementById("status");
-//     var data = new FormData(event.target);
-//     fetch(event.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }).then(response => {
-//         if (!response.ok) {
-//             throw Error(response.statusText);
-//         }
-//         status.classList.add('success');
-//         status.innerHTML = "Thank you!";
-//         form.reset();
-//     }).catch(error => {
-//         status.classList.add('error');
-//         status.innerHTML = "Oops! There was a problem submitting your idea"
-//     });
-// }
-// form.addEventListener("submit", handleSubmit)
-
 // kanye
 let kanye = document.querySelector('.kanye');
 kanye.onmouseover = function () {
     kanye.style.animation = "kanye 1s";
 }
+// module aliases
+const { Engine, Render, World, Bodies, Composites, MouseConstraint } = Matter
+
+Matter.use("matter-wrap")
+
+// get the width/height of the window
+const dpi = window.devicePixelRatio
+const tenTwo = document.querySelector('.ten-two-inner')
+const tenTwoO = document.querySelector('.ten-two')
+const w = tenTwo.offsetWidth
+const h = tenTwoO.offsetHeight
+
+// create an matter.js engine
+const engine = Engine.create()
+
+// create a renderer
+const renderer2 = Render.create({
+    element: tenTwo,
+    engine: engine,
+    options: {
+        width: w,
+        height: h,
+        pixelRatio: dpi,
+        background: 'transparent',
+        wireframes: false,
+    }
+})
+
+const createShape = function (x, y) {
+    const randomNum = Math.random()
+    if (randomNum < 0.05) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            density: 0.04,
+            friction: 1,
+            // frictionAir: 0.00001,
+            // restitution: 0.8,
+            render: {
+                sprite: {
+                    texture: "images/react.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.05 && randomNum < 0.1) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/css.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.1 && randomNum < 0.15) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/html.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.15 && randomNum < 0.2) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/postman.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.2 && randomNum < 0.25) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/styledComponents.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.25 && randomNum < 0.3) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/git.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.3 && randomNum < 0.35) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/figma.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.35 && randomNum < 0.4) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/contentful.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.4 && randomNum < 0.45) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/netlify.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.45 && randomNum < 0.5) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/vanillaJavascript.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.5 && randomNum < 0.55) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/three.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.55 && randomNum < 0.6) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/p5.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.6 && randomNum < 0.65) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/graphQL.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.65 && randomNum < 0.7) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/node.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.7 && randomNum < 0.75) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/gatsby.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.75 && randomNum < 0.8) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/wordpress.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.8 && randomNum < 0.85) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/accessibility.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else if (randomNum > 0.85 && randomNum < 0.89) {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/react.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    } else {
+        return Bodies.rectangle(x, y, 50, 25, {
+            render: {
+                sprite: {
+                    texture: "images/vanillaJavascript.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
+            },
+            plugin: {
+                wrap: {
+                    min: { x: 0, y: 0 },
+                    max: { x: w, y: h }
+                }
+            }
+        })
+    }
+}
+
+const wallOptions = {
+    isStatic: true,
+    render: {
+        visible: false
+    }
+}
+
+const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, wallOptions)
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions)
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions)
+const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 100, wallOptions)
+
+const shapes = Composites.stack(50, 50, 8, 2, 20, 20, function (x, y) {
+    return createShape(x, y)
+})
+
+const bigBall = Bodies.circle(w / 2, h / 2, Math.min(w / 4, h / 4), {
+    isStatic: true,
+    render: {
+        fillStyle: '#000',
+        strokeStyle: 'transparent'
+    }
+})
+// add mouse control
+const mouse = MouseConstraint.create(engine, {
+    element: tenTwo,
+    constraint: {
+        render: {
+            visible: false
+        }
+    }
+})
+
+// add all of the bodies to the world
+World.add(engine.world, [
+    mouse,
+    shapes,
+    bigBall,
+    ground,
+    ceiling,
+    leftWall,
+    rightWall
+])
+// run the engine
+Engine.run(engine)
+// run the renderer
+Render.run(renderer2)
+
+
+tenTwo.addEventListener("click", function (event) {
+    const shape = createShape(event.clientX - 390, event.clientY - 380)
+    World.add(engine.world, shape)
+})
+
+tenTwo.addEventListener("touchstart", function (event) {
+    const shape = createShape(event.pageX, event.pageY)
+    World.add(engine.world, shape)
+})
+
+window.addEventListener('deviceorientation', function (event) {
+    engine.world.gravity = {
+        x: (event.gamma / 50),
+        y: (event.beta / 25)
+    }
+
+    Engine.update(engine)
+})
